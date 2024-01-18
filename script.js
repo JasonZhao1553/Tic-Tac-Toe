65d4504e898350c1983f8982ac9ad1a26230f5bf
Piethegr8 <jzhao1553@gmail.com>
2024-01-17 21:14:35 -0500

    commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the button and modal container elements
+    var button = document.querySelector('[data-reveal-id="Rules"]');
+    // Add a click event listener to the button
+    button.addEventListener('click', function () {
+        alert("Get three in a row.")
+    });
+});
